645c333b55c311783398240e749d5336849ea467
york yao <yaoao12306@outlook.com>
2017-06-16 07:27:56 +0800

    more links

--- common.ts
+++ common.ts
@@ -1,37 +1,48 @@
-export const blogs = [
+export type Post = {
+    id: number;
+    content: string;
+};
+
+export type Blog = {
+    id: number;
+    content: string;
+    posts: Post[];
+};
+
+export const blogs: Blog[] = [
     {
         id: 1,
-        content: "blog 1",
+        content: "blog 1 content",
         posts: [
             {
                 id: 11,
-                content: "post 11",
+                content: "post 11 content",
             },
             {
                 id: 12,
-                content: "post 12",
+                content: "post 12 content",
             },
             {
                 id: 13,
-                content: "post 13",
+                content: "post 13 content",
             },
         ],
     },
     {
         id: 2,
-        content: "blog 2",
+        content: "blog 2 content",
         posts: [
             {
                 id: 21,
-                content: "post 21",
+                content: "post 21 content",
             },
             {
                 id: 22,
-                content: "post 22",
+                content: "post 22 content",
             },
             {
                 id: 23,
-                content: "post 23",
+                content: "post 23 content",
             },
         ],
     },
--- vue/index.ts
+++ vue/index.ts
@@ -1,7 +1,7 @@
 import * as Vue from "vue";
 import Component from "vue-class-component";
 import VueRouter from "vue-router";
-import { blogs } from "../common";
+import * as common from "../common";
 
 Vue.use(VueRouter);
 
@@ -17,30 +17,73 @@ Vue.use(VueRouter);
     </div>
     `,
 })
-class Blogs extends Vue {
-    blogs = blogs;
+class Home extends Vue {
+    blogs = common.blogs;
 }
 
 @Component({
     template: `
-    <div>blog {{$route.params.blog_id}}</div>
+    <div>
+        <div>blog {{blog.id}}</div>
+        <div>{{blog.content}}</div>
+        <div>posts</div>
+        <ul>
+            <li v-for="post in blog.posts">
+                <router-link :to="'/vue/blogs/' + blog.id + '/posts/' + post.id">to /vue/blogs/{{blog.id}}/posts/{{post.id}}</router-link>
+            </li>
+        </ul>
+    </div>
     `,
 })
 class Blog extends Vue {
+    blog: common.Blog;
+
+    beforeMount() {
+        const blogId = +this.$route.params.blog_id;
+        for (const blog of common.blogs) {
+            if (blog.id === blogId) {
+                this.blog = blog;
+                break;
+            }
+        }
+    }
 }
 
 @Component({
     template: `
-    <div>post</div>
+    <div>
+        <div><router-link :to="'/vue/blogs/' + blog.id">to /vue/blogs/{{blog.id}}</router-link></div>
+        <div>post {{post.id}}</div>
+        <div>{{post.content}}</div>
+    </div>
     `,
 })
 class Post extends Vue {
+    post: common.Post;
+    blog: common.Blog;
+
+    beforeMount() {
+        const blogId = +this.$route.params.blog_id;
+        const postId = +this.$route.params.post_id;
+        for (const blog of common.blogs) {
+            if (blog.id === blogId) {
+                this.blog = blog;
+                for (const post of blog.posts) {
+                    if (post.id === postId) {
+                        this.post = post;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
 }
 
 const router = new VueRouter({
     mode: "history",
     routes: [
-        { path: "/vue/blogs", component: Blogs },
+        { path: "/vue", component: Home },
         { path: "/vue/blogs/:blog_id", component: Blog },
         { path: "/vue/blogs/:blog_id/posts/:post_id", component: Post },
     ],
@@ -51,7 +94,7 @@ const router = new VueRouter({
     <div>
         <a href="https://github.com/plantain-00/router-demo/tree/master/vue/index.ts" target="_blank">the source code of the demo</a>
         <br/>
-        <router-link to="/vue/blogs">to /vue/blogs</router-link>
+        <div><router-link to="/vue">home</router-link></div>
         <router-view></router-view>
     </div>
     `,
