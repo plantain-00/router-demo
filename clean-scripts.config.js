e7a3a7f3861387451b056baffb8fe81f9bc9d863
york yao <yaoao12306@outlook.com>
2017-07-28 00:22:50 +0800

    clean scripts

---
+++ clean-scripts.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  build: [
+    `rimraf **/index.min-*.js`,
+    `rimraf dist`,
+    `tsc`,
+    `webpack --config webpack.config.js`,
+    `rev-static --config rev-static.config.js`
+  ],
+  lint: [
+    `tslint "*.ts" "vue/*.ts" "react/*.ts"`,
+    `standard "**/*.config.js"`
+  ],
+  test: [
+    'tsc -p spec',
+    'karma start spec/karma.config.js'
+  ],
+  fix: [
+    `standard --fix "**/*.config.js"`
+  ]
+}
