3121fe75f29e31ee6b248b2b705f59d039f63837
york yao <yaoao12306@outlook.com>
2017-08-14 08:03:58 +0800

    add no-unused-export and watch

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -10,15 +10,17 @@ module.exports = {
     `rev-static --config rev-static.config.js`
   ],
   lint: {
-    ts: `tslint "*.ts" "vue/*.ts" "react/*.ts"`,
-    js: `standard "**/*.config.js"`
+    ts: `tslint "*.ts" "vue/*.ts" "react/*.tsx"`,
+    js: `standard "**/*.config.js"`,
+    export: `no-unused-export "*.ts" "vue/*.ts" "react/*.tsx"`
   },
   test: [
     'tsc -p spec',
     'karma start spec/karma.config.js'
   ],
   fix: {
-    ts: `tslint --fix "*.ts" "vue/*.ts" "react/*.ts"`,
+    ts: `tslint --fix "*.ts" "vue/*.ts" "react/*.tsx"`,
     js: `standard --fix "**/*.config.js"`
-  }
+  },
+  watch: `watch-then-execute "*.ts" "vue/*.ts" "react/*.tsx" --script "npm run build"`
 }
