b3bcc4738a54924f00511e4d27dbcb9b29e09b73
york yao <yaoao12306@outlook.com>
2017-08-21 10:48:54 +0800

    improve watch script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -2,13 +2,9 @@ const childProcess = require('child_process')
 
 module.exports = {
   build: [
-    {
-      js: [
-        `tsc`,
-        `webpack --display-modules --config webpack.config.js`
-      ],
-      clean: `rimraf **/index.min-*.js`
-    },
+    `tsc`,
+    `webpack --display-modules --config webpack.config.js`,
+    `rimraf **/index.min-*.js`,
     `rev-static --config rev-static.config.js`
   ],
   lint: {
@@ -37,5 +33,9 @@ module.exports = {
     ts: `tslint --fix "*.ts" "vue/*.ts" "react/*.tsx"`,
     js: `standard --fix "**/*.config.js"`
   },
-  watch: `watch-then-execute "*.ts" "vue/*.ts" "react/*.tsx" --script "npm run build"`
+  watch: {
+    src: `tsc --watch`,
+    webpack: `webpack --watch`,
+    rev: `rev-static --watch`
+  }
 }
