358e2878e6ea2ac978ecfcb52d2c83aa8cdc58d0
york yao <yaoao12306@outlook.com>
2017-08-26 10:02:54 +0800

    improve appveyor config

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -3,9 +3,9 @@ const childProcess = require('child_process')
 module.exports = {
   build: [
     `tsc`,
-    `webpack --display-modules --config webpack.config.js`,
+    `webpack --display-modules`,
     `rimraf **/index.min-*.js`,
-    `rev-static --config rev-static.config.js`
+    `rev-static`
   ],
   lint: {
     ts: `tslint "*.ts" "vue/*.ts" "react/*.tsx"`,
@@ -14,7 +14,7 @@ module.exports = {
   },
   test: [
     'tsc -p spec',
-    'karma start spec/karma.config.js',
+    process.env.APPVEYOR ? 'echo "skip karma test"' : 'karma start spec/karma.config.js',
     () => new Promise((resolve, reject) => {
       childProcess.exec('git status -s', (error, stdout, stderr) => {
         if (error) {
