d36bae211f4d9fcb1228390be15b5b83789b475a
york yao <yaoao12306@outlook.com>
2017-10-09 14:16:13 +0800

    remove screenshot source; move screenshot to ts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,6 +1,6 @@
 const childProcess = require('child_process')
 const util = require('util')
-const { sleep } = require('clean-scripts')
+const { Service } = require('clean-scripts')
 
 const execAsync = util.promisify(childProcess.exec)
 
@@ -9,37 +9,7 @@ module.exports = {
     `tsc`,
     `webpack --display-modules`,
     `rimraf **/index.min-*.js`,
-    `rev-static`,
-    async () => {
-      const { createServer } = require('http-server')
-      const puppeteer = require('puppeteer')
-      const fs = require('fs')
-      const beautify = require('js-beautify').html
-      const server = createServer({ root: '..' })
-      server.listen(8000)
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
-      await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' })
-
-      for (const type of ['vue', 'react']) {
-        await page.goto(`http://localhost:8000/router-demo/${type}/`)
-        await sleep(500)
-        await page.screenshot({ path: `${type}/screenshot.png`, fullPage: true })
-        fs.writeFileSync(`${type}/screenshot-src.html`, beautify(await page.content()))
-
-        await page.click('li a')
-        await sleep(500)
-        await page.screenshot({ path: `${type}/screenshot-blog.png`, fullPage: true })
-        fs.writeFileSync(`${type}/screenshot-blog-src.html`, beautify(await page.content()))
-
-        await page.click('li a')
-        await sleep(500)
-        await page.screenshot({ path: `${type}/screenshot-blog-post.png`, fullPage: true })
-        fs.writeFileSync(`${type}/screenshot-blog-post-src.html`, beautify(await page.content()))
-      }
-      server.close()
-      browser.close()
-    }
+    `rev-static`
   ],
   lint: {
     ts: `tslint "*.ts" "vue/*.ts" "react/*.tsx"`,
@@ -49,7 +19,6 @@ module.exports = {
   test: [
     'tsc -p spec',
     'karma start spec/karma.config.js',
-    'git checkout "**/screenshot*.png"',
     async () => {
       const { stdout } = await execAsync('git status -s')
       if (stdout) {
@@ -66,5 +35,10 @@ module.exports = {
     src: `tsc --watch`,
     webpack: `webpack --watch`,
     rev: `rev-static --watch`
-  }
+  },
+  screenshot: [
+    new Service(`http-server '..' -p 8000`),
+    `tsc -p screenshots`,
+    `node screenshots/index.js`
+  ]
 }
