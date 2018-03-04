c2f200c78d188cad7d44a4229817ae0a66e799b7
york yao <yaoao12306@outlook.com>
2018-03-04 19:42:00 +0800

    chore: update dependencie

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-const webpack = require('webpack')
-
 module.exports = {
   entry: {
     vue: './vue/index',
@@ -8,21 +6,6 @@ module.exports = {
   output: {
     filename: '[name]/index.min.js'
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      output: {
-        comments: false
-      },
-      exclude: [
-      ]
-    })
-  ],
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
